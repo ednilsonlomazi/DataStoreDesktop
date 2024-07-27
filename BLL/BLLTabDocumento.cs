080e4b199be0d0013629f50f2120e8403388bc7e
ednilson-lomazi <65460604+ednilsonlomazi@users.noreply.github.com>
2024-07-27 10:12:49 -0300

    bll-tab-documento-criada

---
+++ BLL/BLLTabDocumento.cs
@@ -0,0 +1,52 @@
+﻿using DAL;
+using Microsoft.Data.SqlClient;
+using MODEL;
+using System.Data;
+
+namespace BLL
+{
+    public class BLLTabDocumento
+    {
+        private DALConexaoDatabase dalConexaoDatabase;
+        public BLLTabDocumento(DALConexaoDatabase dalConexao)
+        {
+            this.dalConexaoDatabase = dalConexao;
+        }
+
+        public void Insert(TabDocumento tabDocumento)
+        {
+            DALTabDocumento dalTabDocumento = new DALTabDocumento(dalConexaoDatabase);
+            dalTabDocumento.Insert(tabDocumento);
+        }
+
+        public void Update(TabDocumento tabDocumento)
+        {
+
+            if (tabDocumento.codigoStatusDocumento == 6) 
+            {
+                throw new Exception("Documentos Cancelados nao podem ser atualizados");
+            }
+            DALTabDocumento dalTabDocumento = new DALTabDocumento(dalConexaoDatabase);
+            dalTabDocumento.Update(tabDocumento);
+
+        }
+
+        public void Cancelar(TabDocumento tabDocumento)
+        {
+            DALTabDocumento dalTabDocumento = new DALTabDocumento(dalConexaoDatabase);
+            dalTabDocumento.Cancelar(tabDocumento);
+        }
+
+        public DataTable SelectByCod(int codigo)
+        {
+            DALTabDocumento dalTabDocumento = new DALTabDocumento(dalConexaoDatabase);
+            return dalTabDocumento.SelectByCod(codigo);
+        }
+
+        public TabDocumento SelectAll()
+        {
+            DALTabDocumento dalTabDocumento = new DALTabDocumento(dalConexaoDatabase);
+            return dalTabDocumento.SelectAll();
+        }
+    }
+}
